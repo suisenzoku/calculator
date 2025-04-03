16d0a797ff474c21628306bcfc238953da9f8408
suisen <205521266+suisenzoku@users.noreply.github.com>
2025-04-03 14:37:05 -0500

    Handle equals

--- script.js
+++ script.js
@@ -44,7 +44,7 @@ const handleOperatorClick = () => {
     const operators = document.querySelectorAll(".operator");
     operators.forEach((button) => {
         button.addEventListener("click", (e) => {
-            if (operator) {
+            if (operator || !current) {
                 return;
             }
             operator = e.target.id;
@@ -52,6 +52,44 @@ const handleOperatorClick = () => {
             updateDisplay();
         })
     })
+}
+
+const handleEqualsClick = () => {
+    const equals = document.querySelector(".equals");
+    equals.addEventListener('click', (e) => {
+        if (!operator || !current) {
+            return;
+        }
+        const result = evualuateStrings(operator, first, current);
+        if (result) {
+            updateCalculator(result);
+            updateDisplay();
+        } else {
+            clear();
+            updateDisplay("Invalid operation");
+        }
+    });
+}
+
+const evualuateStrings = (operator, a, b) => {
+    const firstValue = Number(a);
+    const secondValue = Number(b);
+    if (isNaN(firstValue) || isNaN(secondValue)) {
+        return null;
+    }
+    return operate(operator, firstValue, secondValue);
+}
+
+const clear = () => {
+    first = "";
+    current = "";
+    opreator = "";
+}
+
+const updateCalculator = (result) => {
+    first = result;
+    current = "";
+    operator = "";
 }
 
 const switchValue = () => {
@@ -66,8 +104,8 @@ const updateDisplay = (text) => {
         return;
     }
 
-    if (operator) {
-        display.textContent = `${first} ${operators[operator]} ${current}`;
+    if (first) {
+        display.textContent = `${first} ${operators[operator] ? operators[operator] : ""} ${current}`;
     } else {
         display.textContent = `${current}`;
     }
@@ -76,6 +114,7 @@ const updateDisplay = (text) => {
 const init = () => {
     handleNumberClick();
     handleOperatorClick();
+    handleEqualsClick();
 }
 
 init();
